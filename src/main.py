006be4822cf1123e6655a5d3f26632d27951e639
Vero2112 <verorodriguezruiz21@gmail.com>
2022-06-20 21:52:04 +0000

    add methods GET,PUT,DELETE

--- src/main.py
+++ src/main.py
@@ -20,9 +20,9 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-todos = [
-    { "text": "My first task", "done": False }
-]
+# todos = [
+#     { "text": "My first task", "done": False }
+# ]
 
 # generate_sitemap con todos los endpoints
 @app.route('/')
@@ -33,8 +33,18 @@ def sitemap():
 
 @app.route('/todos', methods=['GET'])
 def get_task():
-    json_text = jsonify(todos)
-    return json_text
+    # json_text = jsonify(todos) learnpack devuelve asi las task, no mapea
+    # no puedo retornarla directamente, necesito serializarla, hay que transoformala en json. necesito llamar a la funcion list 
+    # y se mapea. En vez de utilizar una función flecha equiv función lambda y va a llamar al serializador y me va
+    # a retornar el serializador
+# esta Task es line 23 models?
+    tasks = Task.query.all()
+    all_tasks = list(map(lambda task: task.serialize(), tasks))
+    # print (tasks)
+    # print("hola")
+    # print(all_tasks)
+    return jsonify(all_tasks)
+    # return json_text
 
 @app.route('/todos', methods=['POST'])
 def create_task():
@@ -42,8 +52,36 @@ def create_task():
 # We already used request.json for that, since we know that the request will be in format application/json. If that is not known, you may want to use request.get_json(force=True) to ignore the content type and treat it like json.
     body = request.get_json()
     print(body)
+# body es un diccionario equivalente a un objeto en JS, text es la key
     task = Task(text=body["text"], done= False)
+# Una vez instanciada la clase, agrego objeto a mi base de datos
     db.session.add(task)
+# confirmamos añadir tarea a mi base de datos, pero no la puedo retornar directamente, para eso necesito serializar
+    db.session.commit()
+# coge un objeto, tipo task y me lo transforma en un diccionario
+# models line 33-37, pq no le ponemos el parametro self?
+    return jsonify(task.serialize())
+
+@app.route('/todos/<int:task_id>', methods=['PUT'])
+def refresh_task(task_id):
+    task = Task.query.get(task_id)
+    print("hola")
+    print(task)
+    body = request.get_json()
+    task.done = body["done"]
+    db.session.commit()
+    return jsonify(task.serialize())
+
+@app.route('/todos/<int:task_id>', methods=['GET'])
+def get_refresh_task(task_id):
+    task = Task.query.get(task_id)
+
+    return jsonify(task.serialize())
+
+@app.route('/todos/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+    db.session.delete(task)
     db.session.commit()
     return jsonify(task.serialize())
 
