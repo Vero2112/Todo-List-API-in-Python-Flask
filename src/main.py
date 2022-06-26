b010a7a1cf7981f2cbbc9869df01897c677b86eb
Vero2112 <verorodriguezruiz21@gmail.com>
2022-06-26 15:47:56 +0000

    Modify APIException

--- src/main.py
+++ src/main.py
@@ -31,7 +31,7 @@ def sitemap():
     return generate_sitemap(app)
     # return "Hola!"
 
-
+# Obtener todas las tareas
 @app.route('/task', methods=['GET'])
 def get_task():
 
@@ -39,7 +39,7 @@ def get_task():
     all_tasks = list(map(lambda task: task.serialize(), tasks))
     return jsonify(all_tasks), 201
 
-
+# Crear una Tarea
 @app.route('/task', methods=['POST'])
 def create_task():
    
@@ -47,8 +47,9 @@ def create_task():
     task = Task(text=body["text"], done= False, user_id=body["user_id"])
     db.session.add(task)
     db.session.commit()
-    return jsonify(task.serialize())
+    return jsonify(task.serialize()),201
 
+# Actualizar una Tarea
 @app.route('/task/<int:task_id>', methods=['PUT'])
 def refresh_task(task_id):
     task = Task.query.get(task_id)
@@ -57,18 +58,21 @@ def refresh_task(task_id):
         raise APIException("Tarea no encontrada", 404)
     body = request.get_json()
     if not ("done" in body):
-        raise APIException("Tarea no encontrada", 404)
+        raise APIException("keyError: done", 404)
     task.done = body["done"]
     db.session.commit()
     return jsonify(task.serialize())
 
+# Obtener una única Tarea
 @app.route('/task/<int:task_id>', methods=['GET'])
 def get_refresh_task(task_id):
+
     task = Task.query.get(task_id)
     if task is None:
         raise APIException("Tarea no encontrada", 404)
     return jsonify(task.serialize())
 
+# Borrar Tarea
 @app.route('/task/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
     task = Task.query.get(task_id)
@@ -78,6 +82,7 @@ def delete_task(task_id):
     db.session.commit()
     return jsonify(task.serialize())
 
+# Obtener todos los usuarios
 @app.route('/user', methods=['GET'])
 def get_all_users():
     
@@ -85,6 +90,7 @@ def get_all_users():
     all_users = list(map(lambda user: user.serialize(), users))
     return jsonify(all_users), 200
 
+# Obtener un único usuario
 @app.route('/user/<user_id>', methods=['GET'])
 def get_user(user_id):
 
@@ -93,7 +99,7 @@ def get_user(user_id):
         raise APIException("Usuario no encontrado", 404)
     return jsonify(user.serialize()), 200
 
-
+# Crear usuario
 @app.route('/user/register', methods=['POST'])
 def create_user():
    
@@ -102,8 +108,7 @@ def create_user():
     user = User(email=body["email"], is_active=True, password=body["password"])
     db.session.add(user)
     db.session.commit()
-    return jsonify(user.serialize())
-
+    return jsonify("Usuario añadido")
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -8,11 +8,9 @@ class User(db.Model):
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
-# transforma la clase en un string, similar a template string de JS
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
 
-# mapeo, transforma el objeto en un diccionario donde tenga key.id con su id, ya no devuelve el objeto (la instancia de una clase) sino que devuelve un diccionario
     def serialize(self):
         return {
             "id": self.id,
@@ -28,11 +26,9 @@ class Task(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     user = db.relationship(User)
 
-# transforma la clase en un string, similar a template string de JS
     def __repr__(self):
         return '<Task %r>' % self.text
 
-# mapeo, transforma el objeto en un diccionario donde tenga key.id con su id, ya no devuelve el objeto (la instancia de una clase) sino que devuelve un diccionario
     def serialize(self):
         return {
             "id": self.id,
